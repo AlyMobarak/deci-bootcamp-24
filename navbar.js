eccbba607cf198fdf31a2140f0284edeb3c72621
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-08-08 00:54:11 +0300

    d

---
+++ navbar.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarToggle = document.getElementById("navbarToggle");
+  const navbarLinks = document.getElementById("navbarLinks");
+
+  navbarToggle.addEventListener("click", function () {
+    navbarLinks.classList.toggle("active");
+  });
+});
